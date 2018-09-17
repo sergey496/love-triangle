c6c75c6acf69ef63bad58bc60f163c775408701e
Sergey Zylevich <sergey496@yandex.ru>
2018-09-17 15:14:26 +0300

    17.09

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
 	let count=0;
 	for (let i=1;i<preferences.length+1;i++){
 		if (preferences[i-1]!=preferences[preferences[i-1]-1]){
-			if (i==(preferences[preferences[preferences[i-1]-1]-1]))
-			{
+			if (i==(preferences[preferences[preferences[i-1]-1]-1])){
 				count=count+1;
 				preferences[i-1]="";
 			}
