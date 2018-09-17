c26b9f9a10cbdba5ff1cce94590ccfbecb3177cf
Sergey Zylevich <sergey496@yandex.ru>
2018-09-17 15:08:23 +0300

    17.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.201817.09.2018

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-var count=0;
-for (var i=1;i<preferences.length+1;i++){
-	if(preferences[i-1]!=preferences[preferences[i-1]-1]){
-		if (i==(preferences[preferences[preferences[i-1]-1]-1]))
-		{
-			count=count+1;
-			preferences[i-1]="";
-		}
-	}	
-}
-
-
-return count;
+	let count=0;
+	for (let i=1;i<preferences.length+1;i++){
+		if (preferences[i-1]!=preferences[preferences[i-1]-1]){
+			if (i==(preferences[preferences[preferences[i-1]-1]-1]))
+			{
+				count=count+1;
+				preferences[i-1]="";
+			}
+		}	
+	}
+	return count;
 };
